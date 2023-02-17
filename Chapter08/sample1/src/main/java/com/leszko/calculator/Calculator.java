7f652306822062a1e48019d466ad407be41a1bf9
Ben Sieradzki <benjamin_sieradzki@student.uml.edu>
2023-02-17 13:26:36 -0500

    maybe above the decorator for comment?

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,11 +3,11 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-@Service
 /**
 * A public Calculator Service
 * javadoc comments need to be multiline I guess
 */
+@Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
 	@Cacheable("sum")
