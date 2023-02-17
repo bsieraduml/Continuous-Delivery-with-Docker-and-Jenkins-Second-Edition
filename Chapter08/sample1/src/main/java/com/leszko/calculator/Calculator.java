c62c9c54406d0858833e75f9f1e55c2e0b2d0a58
Ben Sieradzki <benjamin_sieradzki@student.uml.edu>
2023-02-17 12:49:43 -0500

    add a comment to the public class definition to fix checkstyle pipeline error

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,7 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+//this comment should stop my pipeline from failing; look it's a public class, makes sense
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
